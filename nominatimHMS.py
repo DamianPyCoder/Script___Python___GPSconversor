869b48d001c286213383b9df8c235af41bff7eb4
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2023-07-03 21:29:11 +0200

    Update nominatimHMS.py ahora pide por consola los datos

--- nominatimHMS.py
+++ nominatimHMS.py
@@ -21,14 +21,16 @@ def obtener_direccion(grados_lat, minutos_lat, segundos_lat, grados_lon, minutos
     
     return f"Calle: {calle}, Número: {numero}"
 
-# Ejemplo de uso
-grados_lat = 41
-minutos_lat = 23
-segundos_lat = 30
+# Solicitar coordenadas al usuario
+grados_lat = float(input("Introduce los grados de latitud: "))
+minutos_lat = float(input("Introduce los minutos de latitud: "))
+segundos_lat = float(input("Introduce los segundos de latitud: "))
 
-grados_lon = 2
-minutos_lon = 10
-segundos_lon = 15
+grados_lon = float(input("Introduce los grados de longitud: "))
+minutos_lon = float(input("Introduce los minutos de longitud: "))
+segundos_lon = float(input("Introduce los segundos de longitud: "))
 
+# Obtener dirección y mostrar el resultado
 direccion = obtener_direccion(grados_lat, minutos_lat, segundos_lat, grados_lon, minutos_lon, segundos_lon)
 print("La dirección es:", direccion)
+
